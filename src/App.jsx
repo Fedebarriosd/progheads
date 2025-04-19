01d138c5382624bffaed665d58394c163875a560
Fede Barrios <fedebarriosd@gmail.com>
2025-04-19 14:59:36 -0300

    Test 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/progheads.css';
+import ProgheadsLanding from './components/ProgheadsLanding';
+
+function App() {
+  return (
+    <ProgheadsLanding />
+  );
+}
+
+export default App;
