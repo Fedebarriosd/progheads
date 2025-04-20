69504185f7c83c9c0b5462ae9abfef212012ca7d
Fede Barrios <fedebarriosd@gmail.com>
2025-04-20 14:23:39 -0300

    forzar redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles/progheads.css';
 import ProgheadsLanding from './components/ProgheadsLanding';
-
+//test
 function App() {
   return (
     <ProgheadsLanding />
