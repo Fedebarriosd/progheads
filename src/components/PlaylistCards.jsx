42a94943f2c91deac542c3594f3c6d5c1bdaf4d0
Fede Barrios <fedebarriosd@gmail.com>
2025-06-10 11:52:51 -0300

    Actualización para SEO

--- src/components/PlaylistCards.jsx
+++ src/components/PlaylistCards.jsx
@@ -7,28 +7,32 @@ const playlists = [
     title: 'Esenciales Prog Rock',
     cover: '/Esenciales-progrock.png',
     url: 'https://open.spotify.com/playlist/19U1kI7h1DrtXIgoMIrI8O?si=377e2ca7125945b7',
-    description: 'Las piezas fundamentales del prog clásico: épica, poesía británica y teclados interminables.'
+    description: 'Las piezas fundamentales del prog clásico: épica, poesía británica y teclados interminables.',
+    alt: 'Portada de la playlist Esenciales Prog Rock'
   },
   {
     id: 2,
     title: 'Esenciales Prog Metal',
     cover: '/Esenciales-progmetal.png',
     url: 'https://open.spotify.com/playlist/4IVt1rwTIvJ63PHPD3Sm1q?si=a15d0ef1be29428b',
-    description: 'Como el metal normal, pero con más culpa, tiempos raros y menos fans.'
+    description: 'Como el metal normal, pero con más culpa, tiempos raros y menos fans.',
+    alt: 'Portada de la playlist Esenciales Prog Metal'
   },
   {
     id: 3,
     title: 'Gemas Ocultas Prog Rock',
     cover: '/Joyas-progrock.png',
     url: 'https://open.spotify.com/playlist/6FKuiIF9pYWcOCM7QRrJHN?si=3eb0267b7baf4e69',
-    description: 'Bandas olvidadas, discos raros, joyas que nunca llegaron al mainstream… hasta ahora.'
+    description: 'Bandas olvidadas, discos raros, joyas que nunca llegaron al mainstream… hasta ahora.',
+    alt: 'Portada de la playlist Gemas Ocultas Prog Rock'
   },
   {
     id: 4,
     title: 'Gemas Ocultas Prog Metal',
     cover: '/Joyas-progmetal.png',
     url: 'https://open.spotify.com/playlist/3PuQ0wXNOML7M7AFIaXu39?si=f42ef1ff9a8e44a2',
-    description: 'Solos interminables, growls existenciales y estructuras que desafían toda lógica... y popularidad.'
+    description: 'Solos interminables, growls existenciales y estructuras que desafían toda lógica... y popularidad.',
+    alt: 'Portada de la playlist Gemas Ocultas Prog Metal'
   }
 ];
 
